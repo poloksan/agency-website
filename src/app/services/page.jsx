e8d65ea68d6a8336074b3a86b303b9b497a2048a
Polok Banik <polokbanik7@gmail.com>
2026-02-04 20:02:01 +0600

    add service secton

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -1,75 +1,102 @@
-import React from 'react';
-import { GrUserManager } from 'react-icons/gr';
-import { HiMiniArrowRight } from 'react-icons/hi2';
+import Link from "next/link";
+import React from "react";
+import Marquee from "react-fast-marquee";
 
 const services = [
     {
-        id: 1,
-        title: 'Tech Innovation',
-        para: 'It is a long established fact that a reader will be distracted by the readable content of a page when',
+        title: "design",
+        subTitle: "Lorem ipsum dolor sit.",
     },
     {
-        id: 2,
-        title: 'Network security',
-        para: 'It is a long established fact that a reader will be distracted by the readable content of a page when',
+        title: "marketing",
+        subTitle: "Lorem ipsum dolor sit.",
     },
     {
-        id: 3,
-        title: 'Data Analysis',
-        para: '',
-    },
-
-    {
-        id: 4,
-        title: 'Tech Innovation',
-        para: 'It is a long established fact that a reader will be distracted by the readable content of a page when',
+        title: "prototype",
+        subTitle: "Lorem ipsum dolor sit.",
     },
     {
-        id: 5,
-        title: 'Network security',
-        para: 'It is a long established fact that a reader will be distracted by the readable content of a page when',
-    },
-    {
-        id: 6,
-        title: 'Data Analysis',
-        para: '',
+        title: "branding",
+        subTitle: "Lorem ipsum dolor sit.",
     },
 ];
 
 export default function ServicesSection() {
     return (
-        <section className="w-full ">
-            <div className="w-full max-w-360 h-auto mx-auto px-8 sm:px-16 xl:py-30 flex flex-col items-center justify-center xl:gap-12 relative">
-                <h1>service</h1>
+        <section className="w-full overflow-hidden">
+            <div className="w-full flex flex-col items-center justify-center xl:gap-12 relative">
+                {/* heading */}
+                <div className="w-full max-w-360 mx-auto px-8 sm:px-16 py-20 flex flex-col items-center space-y-5">
+                    <h5 className="border-2 border-custom-primary/50 inline-block px-6 py-2 rounded-full text-custom-light-gray text-sm md:text-base lg:text-xl uppercase select-none shadow-amber-400">
+                        services
+                    </h5>
 
-                <div className="">
-                    <div className="w-full max-w-[500px] space-y-12 bg-custom-black lg:p-12.5 border border-custom-white/15 rounded-[40px]">
-                        {/* icon */}
-                        <div className="w-full flex items-center justify-between">
-                            <div className="w-28 h-28 rounded-full bg-custom-dark-gray/30 grid place-items-center">
-                                <GrUserManager className="text-custom-primary text-5xl" />
-                            </div>
+                    <div className="w-full flex items-center justify-between">
+                        <h1 className="max-w-120 text-7xl font-bold leading-20 tracking-tight">
+                            Lorem ipsum dolor sit.
+                        </h1>
 
-                            <p>01</p>
-                        </div>
-
-                        {/* text content */}
-                        <div>
-                            <h4>Tech Information</h4>
-                            <p>
-                                It is a long established fact that a reader will be distracted by
-                                the readable content of a page when
-                            </p>
-                        </div>
-
-                        {/* btn */}
-                        <div>
-                            <HiMiniArrowRight />
-                            <button>Read More</button>
-                            <HiMiniArrowRight />
-                        </div>
+                        <p className="w-full max-w-95 text-xl text-custom-white/70 leading-8 text-right">
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Vel quas exercitationem quaerat esse, iusto
+                            dignissimos nihil eaque id minima accusamus!
+                        </p>
                     </div>
                 </div>
+
+                <Marquee
+                    autoFill
+                    direction="right"
+                    loop={0}
+                    speed={70}
+                    className="py-20 -mt-20 flex items-center overflow-visible"
+                >
+                    {/* card content */}
+                    {services.map((service, i) => {
+                        const { title, subTitle } = service;
+                        return (
+                            <div key={i} className="mx-10">
+                                {/* card */}
+                                <div className="relative group ">
+                                    {/* under card */}
+                                    <div className="border-[0.5px] rounded-3xl w-110 h-110 relative ">
+                                        {/* blob */}
+                                        <div className="h-40 w-40 bg-custom-primary/30 rounded-full blur-[90px] absolute top-10 right-10 animate-pulse"></div>
+                                        <div className="absolute bottom-10 left-10 space-y-5 pointer-events-none">
+                                            <h4 className="text-7xl font-semibold capitalize">
+                                                {title}
+                                            </h4>
+                                            <h6 className="text-lg uppercase">
+                                                {subTitle}
+                                            </h6>
+                                        </div>
+                                    </div>
+                                    {/* over card */}
+                                    <div className="w-110 h-110 rounded-3xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-custom-black bg-custom-primary opacity-0 scale-0 origin-bottom-left group-hover:scale-100 group-hover:opacity-100 duration-600 transition-all ">
+                                        {/* <div className="h-40 w-40 bg-custom-primary/30 rounded-full blur-[90px] absolute top-0 right-0 animate-pulse"></div> */}
+                                        <div className="absolute top-10 right-10 space-y-5 pointer-events-none">
+                                            <h4 className="text-7xl font-semibold capitalize">
+                                                {title}
+                                            </h4>
+                                            <h6 className="text-lg uppercase">
+                                                {subTitle}
+                                            </h6>
+                                        </div>
+
+                                        <Link href={"/"}>
+                                            <button
+                                                type="button"
+                                                className="border-2 px-6 py-2 text-2xl font-medium rounded-xl absolute bottom-10 left-10 cursor-pointer hover:bg-custom-black hover:text-custom-white hover:border-custom-black duration-300"
+                                            >
+                                                Show More
+                                            </button>
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </Marquee>
             </div>
         </section>
     );
