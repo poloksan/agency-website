aa6778a5813a4ff080648ff8edcbdcceab119ad8
poloksan <polok.sanmo@gmail.com>
2026-02-12 20:11:34 +0600

    fixed some bug in hero section

--- src/components/home/hero/HeroSection.jsx
+++ src/components/home/hero/HeroSection.jsx
@@ -58,14 +58,14 @@ export default function HeroSection() {
                 className="hidden lg:block absolute top-0 right-0  pointer-events-none animate-pulse"
             />
 
-            <div className="w-full h-[80vh] md:h-screen max-w-360 mx-auto px-8 sm:px-16 pb-11 pt-36 lg:pt-30 flex flex-col items-center justify-center gap-6 xl:gap-20 relative">
+            <div className="w-full h-[80vh] sm:h-screen max-w-360 mx-auto px-8 sm:px-16 pb-11 pt-36 lg:pt-30 flex flex-col items-center justify-center gap-7 sm:gap-10  xl:gap-20 relative">
                 <motion.h1
                     initial={{
                         x: 100,
                         opacity: 0,
                     }}
                     animate={heroH1Control}
-                    className="w-full tracking-[-2px] font-light text-4xl md:text-7xl lg:text-[100px] xl:text-[150px] xl:leading-10 flex lg:flex-row items-center gap-4 md:gap-8 xl:gap-10 text-center"
+                    className="w-full tracking-[-2px] font-light text-3xl sm:text-6xl md:text-7xl lg:text-[100px] xl:text-[150px] xl:leading-10 flex lg:flex-row items-center justify-between gap-4 md:gap-8 xl:gap-10 text-center"
                 >
                     Creating
                     <motion.div
@@ -74,7 +74,7 @@ export default function HeroSection() {
                             opacity: 0,
                         }}
                         animate={heroH1ControlMarquee}
-                        className="w-full max-w-60 md:max-w-100 lg:max-w-110 xl:max-w-170 bg-custom-primary select-none lg:h-28 xl:h-40 rounded-full text-custom-black overflow-hidden flex flex-col justify-center"
+                        className="w-full max-w-60 sm:max-w-80 md:max-w-100 lg:max-w-110 xl:max-w-170 bg-custom-primary select-none lg:h-28 xl:h-40 rounded-full text-custom-black overflow-hidden flex flex-col justify-center"
                     >
                         <Marquee
                             direction="right"
@@ -82,7 +82,7 @@ export default function HeroSection() {
                             speed={50}
                             autoFill
                             pauseOnHover
-                            className="w-full h-full text-4xl md:text-6xl lg:text-[90px] xl:text-[140px] xl:leading-10 tracking-[-2px] font-light text-center"
+                            className="w-full h-full text-3xl sm:text-6xl md:text-6xl lg:text-[90px] xl:text-[140px] xl:leading-10 tracking-[-2px] font-light text-center"
                         >
                             <span className="mx-2 lg:mx-5">brands</span>
                         </Marquee>
@@ -94,9 +94,9 @@ export default function HeroSection() {
                         opacity: 0,
                     }}
                     animate={heroMiddleTextControl}
-                    className="w-full flex items-center justify-between"
+                    className="w-full flex items-center justify-between -mb-3"
                 >
-                    <h1 className="w-full text-4xl md:text-7xl lg:text-[100px] xl:text-[150px] tracking-[-2px] font-light xl:leading-10 flex items-center gap-4 lg:gap-10 text-center -mt-16 sm:mt-0">
+                    <h1 className="w-full text-3xl sm:text-6xl md:text-7xl lg:text-[100px] xl:text-[150px] tracking-[-2px] font-light xl:leading-10 flex items-center gap-4 lg:gap-10 text-center -mt-16 sm:-mt-10 md:-mt-16 lg:-mt-10">
                         and digital
                     </h1>
                     <motion.p
@@ -105,13 +105,12 @@ export default function HeroSection() {
                             opacity: 0,
                         }}
                         animate={heroMiddleTextControlDelay}
-                        className="text-custom-white/70 w-full md:max-w-56 lg:max-w-100 text-xs md:text-lg xl:text-[22px] leading-4 md:leading-8 text-right lg:text-start -mt-4"
+                        className="text-custom-white/70 w-full md:max-w-56 lg:max-w-100 text-xs sm:text-sm md:text-lg xl:text-[22px] leading-4 md:leading-8 text-right lg:text-start -mt-4"
                     >
                         We design exceptional brands, products, web apps, mobile
                         apps,websites for startups and enterprises.
                     </motion.p>
                 </motion.div>
-
                 <motion.div
                     initial={{
                         x: 100,
@@ -126,7 +125,7 @@ export default function HeroSection() {
                             speed={45}
                             autoFill
                             pauseOnHover
-                            className="w-full max-w-60 md:max-w-100 text-4xl md:text-7xl lg:text-[90px] xl:text-[140px] xl:leading-10 tracking-[-2px] font-light text-center bg-custom-primary lg
+                            className="w-full max-w-60 sm:max-w-80 md:max-w-100 text-3xl sm:text-6xl md:text-7xl lg:text-[90px] xl:text-[140px] xl:leading-10 tracking-[-2px] font-light text-center bg-custom-primary lg
                         lg:max-w-110 xl:max-w-170 select-none lg:h-28 xl:h-40 rounded-full text-custom-black  overflow-hidden"
                         >
                             <span className="mx-2 lg:mx-5">solutions</span>
