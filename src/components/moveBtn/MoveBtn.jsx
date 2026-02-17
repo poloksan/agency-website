65956addb56465c13656b95bff6ca47220c00d79
polok-sanmo <polok.sanmo@gmail.com>
2026-02-17 10:22:08 +0600

    link the cta button

--- src/components/moveBtn/MoveBtn.jsx
+++ src/components/moveBtn/MoveBtn.jsx
@@ -5,7 +5,7 @@ import { BsArrowUpRight } from 'react-icons/bs';
 export default function MoveBtn() {
     return (
         <Link
-            href={'/'}
+            href={'/contact'}
             className="border-2 border-custom-white/50 w-25 h-25 md:w-32 md:h-32 xl:w-40 xl:h-40 rounded-full flex flex-col items-center justify-center gap-2 text-sm md:text-xl font-semibold transition-all duration-500 group bg-custom-black/20 backdrop-blur-sm"
         >
             <BsArrowUpRight className="text-lg xl:text-2xl group-hover:rotate-45 duration-500" />
